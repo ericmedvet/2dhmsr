85bd821a20c41f3b4dfcc11cb33b5632335a63d8
unknown <giorgia.nadizar@gmail.com>
2021-03-13 10:43:21 +0100

    ActivationFunction now implements Function<Double,Double>

--- src/main/java/it/units/erallab/hmsrobots/core/controllers/MultiLayerPerceptron.java
+++ src/main/java/it/units/erallab/hmsrobots/core/controllers/MultiLayerPerceptron.java
@@ -31,7 +31,7 @@ import java.util.stream.Collectors;
  */
 public class MultiLayerPerceptron implements Serializable, RealFunction, Parametrized {
 
-  public enum ActivationFunction {
+  public enum ActivationFunction implements Function<Double,Double>{
     RELU(x -> (x < 0) ? 0d : x),
     SIGMOID(x -> 1d / (1d + Math.exp(-x))),
     SIN(Math::sin),
@@ -45,6 +45,10 @@ public class MultiLayerPerceptron implements Serializable, RealFunction, Paramet
 
     public Function<Double, Double> getF() {
       return f;
+    }
+
+    public Double apply(Double x){
+      return f.apply(x);
     }
   }
 
