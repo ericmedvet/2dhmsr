0ae141bf6228f96ce1dd591d8ee8ee86a6cc5d3f
unknown <giorgia.nadizar@gmail.com>
2021-03-18 18:26:26 +0100

    Range of value to spike train converters updated to [0-1].

--- src/main/java/it/units/erallab/hmsrobots/core/controllers/snn/converters/vts/ValueToSpikeTrainConverter.java
+++ src/main/java/it/units/erallab/hmsrobots/core/controllers/snn/converters/vts/ValueToSpikeTrainConverter.java
@@ -7,7 +7,7 @@ import java.util.SortedSet;
 
 public interface ValueToSpikeTrainConverter extends Serializable, Resettable {
 
-  double LOWER_BOUND = -1;
+  double LOWER_BOUND = 0;
   double UPPER_BOUND = 1;
   double DEFAULT_FREQUENCY = 600;
 
@@ -20,11 +20,8 @@ public interface ValueToSpikeTrainConverter extends Serializable, Resettable {
   }
 
   default double normalizeValue(double value) {
-    //if (value > UPPER_BOUND || value < LOWER_BOUND) {
-    //  throw new IllegalArgumentException(String.format("Expected input in range [%f,%f]: found %f", LOWER_BOUND, UPPER_BOUND, value));
-    //}
-    value = Math.max(Math.min(UPPER_BOUND, value), LOWER_BOUND);
-    return (1 + value) / 2;
+    return Math.max(Math.min(UPPER_BOUND, value), LOWER_BOUND);
+    // return (1 + value) / 2;
   }
 
 }
