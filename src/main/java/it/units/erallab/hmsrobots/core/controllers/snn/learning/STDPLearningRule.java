f58303ac32c13441d3e0df09abfe34e9bbcef176
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-09-20 16:19:42 +0200

    Learning rules can now be created with default values.

--- src/main/java/it/units/erallab/hmsrobots/core/controllers/snn/learning/STDPLearningRule.java
+++ src/main/java/it/units/erallab/hmsrobots/core/controllers/snn/learning/STDPLearningRule.java
@@ -28,6 +28,7 @@ public abstract class STDPLearningRule implements Parametrized, Serializable {
     this.aMinus = aMinus;
   }
 
+
   private static STDPLearningRule createFromReals(double[] parameters) {
     if (parameters.length != 6) {
       throw new IllegalArgumentException(String.format("Expected 6 parameters, received %d", parameters.length));
@@ -54,8 +55,38 @@ public abstract class STDPLearningRule implements Parametrized, Serializable {
     return stdpLearningRule;
   }
 
+  private static STDPLearningRule createFromReals(double[] parameters, double[] defaultSymmetricParams, double[] defaultAsymmetricParams){
+    if (parameters.length != 2) {
+      throw new IllegalArgumentException(String.format("Expected 2 parameters, received %d", parameters.length));
+    }
+    double[] stdpParams;
+    STDPLearningRule stdpLearningRule;
+    // first parameter regulates symmetry, second regulates hebbian/antihebbian
+    if (parameters[0] > 0) {
+      stdpParams = SymmetricSTPDLearningRule.scaleParameters(defaultSymmetricParams);
+      if (parameters[1] > 0) {
+        stdpLearningRule = new SymmetricHebbianLearningRule();
+      } else {
+        stdpLearningRule = new SymmetricAntiHebbianLearningRule();
+      }
+    } else {
+      stdpParams = AsymmetricSTDPLearningRule.scaleParameters(defaultAsymmetricParams);
+      if (parameters[1] > 0) {
+        stdpLearningRule = new AsymmetricHebbianLearningRule();
+      } else {
+        stdpLearningRule = new AsymmetricAntiHebbianLearningRule();
+      }
+    }
+    stdpLearningRule.setParams(stdpParams);
+    return stdpLearningRule;
+  }
+
   public static STDPLearningRule[] createLearningRules(double[][] params) {
     return Arrays.stream(params).map(p -> STDPLearningRule.createFromReals(p)).toArray(STDPLearningRule[]::new);
+  }
+
+  public static STDPLearningRule[] createLearningRules(double[][] params, double[] defaultSymmetricParams, double[] defaultAsymmetricParams) {
+    return Arrays.stream(params).map(p -> STDPLearningRule.createFromReals(p,defaultSymmetricParams,defaultAsymmetricParams)).toArray(STDPLearningRule[]::new);
   }
 
   public abstract double computeDeltaW(double deltaT);
