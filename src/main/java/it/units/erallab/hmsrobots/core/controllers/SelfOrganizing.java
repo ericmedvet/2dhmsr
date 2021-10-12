809d3fb6cee0c7594ee31a1c406064873c1fad72
federicopigozzi <pigozzife@gmail.com>
2021-10-12 16:28:40 +0200

    copyright

--- src/main/java/it/units/erallab/hmsrobots/core/controllers/SelfOrganizing.java
+++ src/main/java/it/units/erallab/hmsrobots/core/controllers/SelfOrganizing.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2021 Federico Pigozzi <pigozzife@gmail.com> (as Federico Pigozzi <pigozzife@gmail.com>)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.units.erallab.hmsrobots.core.controllers;
 
 import com.fasterxml.jackson.annotation.*;
@@ -9,7 +25,9 @@ import java.io.Serializable;
 import java.util.*;
 import java.util.stream.Collectors;
 
-
+/**
+ * @author federico
+ */
 public class SelfOrganizing implements Controller<SensingVoxel> {
 
   public static class Edge implements Serializable {
