b7b0a0631e516c752546f3e2bb458fd81e586463
Giulia Marchiori Pietrosanti <48836873+GiuliaMP@users.noreply.github.com>
2021-12-20 11:26:18 +0100

    Changed the order of index in keyPressed and keyReleased

--- src/main/java/it/units/erallab/hmsrobots/core/controllers/BasicInteractiveController.java
+++ src/main/java/it/units/erallab/hmsrobots/core/controllers/BasicInteractiveController.java
@@ -10,14 +10,13 @@ import java.util.*;
 public class BasicInteractiveController extends AbstractController<ControllableVoxel> {
 
   private List<Boolean> isKeyPressed;
-  private List<Collection<Pair<Integer, Integer>>> voxelDivision;
+  List<Set<Grid.Key>>  poses;
 
   public BasicInteractiveController() {
     isKeyPressed = new ArrayList<>();
     for (int i = 0; i<4; i++) {
       isKeyPressed.add(false);
     }
-    System.out.println("hello");
   }
 
   private Grid<Boolean> getShape(Grid<? extends ControllableVoxel> voxels) {
@@ -31,14 +30,11 @@ public class BasicInteractiveController extends AbstractController<ControllableV
   @Override
   public Grid<Double> computeControlSignals(double t, Grid<? extends ControllableVoxel> voxels) {
     Grid<Boolean> shape = getShape(voxels);
-    List<Set<Grid.Key>> poses = new ArrayList<>(PoseUtils.computeCardinalPoses(shape));
+    poses = new ArrayList<>(PoseUtils.computeCardinalPoses(shape));
     Grid<Double> values = Grid.create(voxels, v -> 1d);
     for (int i = 0; i < isKeyPressed.size(); i++) {
       for (Grid.Key key : poses.get(i)) {
         values.set(key.getX(), key.getY(), isKeyPressed.get(i)?-1d:1d);
-        /*if (key.getX() >= 0 && key.getX() < values.getW() && key.getY() >= 0 && key.getY() < values.getH()) {
-          values.set(key.getX(), key.getY(), isKeyPressed[i]?-1d:1d);
-        }*/
       }
     }
     return values;
--- src/main/java/it/units/erallab/hmsrobots/core/snapshots/InteractiveSnapshotListener.java
+++ src/main/java/it/units/erallab/hmsrobots/core/snapshots/InteractiveSnapshotListener.java
@@ -105,16 +105,16 @@ public class InteractiveSnapshotListener extends JFrame implements SnapshotListe
   public void keyPressed(KeyEvent e) {
     switch(e.getKeyCode()){
       case KeyEvent.VK_UP:
-        controller.setKeyPressed(true, 0);
+        controller.setKeyPressed(true, 2);
         break;
       case KeyEvent.VK_DOWN:
-        controller.setKeyPressed(true, 3);
-        break;
-      case KeyEvent.VK_LEFT:
         controller.setKeyPressed(true, 1);
         break;
+      case KeyEvent.VK_LEFT:
+        controller.setKeyPressed(true, 0);
+        break;
       case KeyEvent.VK_RIGHT:
-        controller.setKeyPressed(true, 2);
+        controller.setKeyPressed(true, 3);
         break;
       default:
         System.out.println("key pressed: not an arrow");
@@ -129,16 +129,16 @@ public class InteractiveSnapshotListener extends JFrame implements SnapshotListe
   public void keyReleased(KeyEvent e) {
     switch(e.getKeyCode()){
       case KeyEvent.VK_UP:
-        controller.setKeyPressed(false, 0);
+        controller.setKeyPressed(false, 2);
         break;
       case KeyEvent.VK_DOWN:
-        controller.setKeyPressed(false, 3);
-        break;
-      case KeyEvent.VK_LEFT:
         controller.setKeyPressed(false, 1);
         break;
+      case KeyEvent.VK_LEFT:
+        controller.setKeyPressed(false, 0);
+        break;
       case KeyEvent.VK_RIGHT:
-        controller.setKeyPressed(false, 2);
+        controller.setKeyPressed(false, 3);
         break;
       default:
         System.out.println("key released: not an arrow");
